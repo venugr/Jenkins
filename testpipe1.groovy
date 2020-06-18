af73a01d0d1fca58fc5174c1e9b8344606d5bc28
venugr <venugr@yahoo.com>
2020-06-18 16:53:17 +0530

    Update testpipe1.groovy

--- testpipe1.groovy
+++ testpipe1.groovy
@@ -1,10 +1,12 @@
 pipeline {
     agent any
+    /*
     parameters {
         choice(name: 'Environment',choices: "Dev\nTest\nProd", description: 'interesting stuff' )
         string(name: 'Client', defaultValue: 'rmu-test', description: 'How should I greet the world?')
         booleanParam(defaultValue: false, name: 'ProceedWithBuild', description: 'Process option 1')
     }
+    */
     parameters([
         [$class: 'ChoiceParameter', 
             choiceType: 'PT_SINGLE_SELECT', 
