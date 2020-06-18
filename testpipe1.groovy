5740765a173688b59d51934920762923af661c09
venugr <venugr@yahoo.com>
2020-06-18 16:58:23 +0530

    Update testpipe1.groovy

--- testpipe1.groovy
+++ testpipe1.groovy
@@ -1,12 +1,4 @@
-pipeline {
-    agent any
-    /*
-    parameters {
-        choice(name: 'Environment',choices: "Dev\nTest\nProd", description: 'interesting stuff' )
-        string(name: 'Client', defaultValue: 'rmu-test', description: 'How should I greet the world?')
-        booleanParam(defaultValue: false, name: 'ProceedWithBuild', description: 'Process option 1')
-    }
-    */
+properties([
     parameters([
         [$class: 'ChoiceParameter', 
             choiceType: 'PT_SINGLE_SELECT', 
@@ -30,7 +22,55 @@ pipeline {
                         'return["Dev","QA","Stage","Prod"]'
                 ]
             ]
-        ]])
+        ], 
+        [$class: 'CascadeChoiceParameter', 
+            choiceType: 'PT_SINGLE_SELECT', 
+            description: 'Select the Server from the Dropdown List', 
+            filterLength: 1, 
+            filterable: true, 
+            name: 'Server', 
+            randomName: 'choice-parameter-5631314456178619', 
+            referencedParameters: 'Env', 
+            script: [
+                $class: 'GroovyScript', 
+                fallbackScript: [
+                    classpath: [], 
+                    sandbox: false, 
+                    script: 
+                        'return[\'Could not get Environment from Env Param\']'
+                ], 
+                script: [
+                    classpath: [], 
+                    sandbox: false, 
+                    script: 
+                        ''' if (Env.equals("Dev")){
+                                return["devaaa001","devaaa002","devbbb001","devbbb002","devccc001","devccc002"]
+                            }
+                            else if(Env.equals("QA")){
+                                return["qaaaa001","qabbb002","qaccc003"]
+                            }
+                            else if(Env.equals("Stage")){
+                                return["staaa001","stbbb002","stccc003"]
+                            }
+                            else if(Env.equals("Prod")){
+                                return["praaa001","prbbb002","prccc003"]
+                            }
+                        '''
+                ]
+            ]
+        ]
+    ])
+])
+
+pipeline {
+    agent any
+    /*
+    parameters {
+        choice(name: 'Environment',choices: "Dev\nTest\nProd", description: 'interesting stuff' )
+        string(name: 'Client', defaultValue: 'rmu-test', description: 'How should I greet the world?')
+        booleanParam(defaultValue: false, name: 'ProceedWithBuild', description: 'Process option 1')
+    }
+    */
     
     stages {
         stage('Example') {
