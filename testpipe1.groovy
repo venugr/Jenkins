d34c35c7b9084c1f12ef0a7ef9e8c56bc54c40d8
venugr <venugr@yahoo.com>
2020-06-18 16:35:11 +0530

    Update testpipe1.groovy

--- testpipe1.groovy
+++ testpipe1.groovy
@@ -3,7 +3,7 @@ pipeline {
     parameters {
         choice(name: 'Environment',choices: "Dev\nTest\nProd", description: 'interesting stuff' )
         string(name: 'Client', defaultValue: 'rmu-test', description: 'How should I greet the world?')
-        booleanParam(defaultValue: false, name: 'ProceedWithBuild', description: 'Process option 1'),
+        booleanParam(defaultValue: false, name: 'ProceedWithBuild', description: 'Process option 1')
         
     }
     stages {
